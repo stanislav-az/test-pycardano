30cf6d3998755ec6764f5267b858065ebe86d5e1
Stanislav Zhdanovich <st.zhdanovich@gmail.com>
2023-07-13 17:40:26 +0700

    print owners pkh

--- hd_wallet_owner.py
+++ hd_wallet_owner.py
@@ -10,6 +10,7 @@ hd_wallet = HDWallet.from_mnemonic(MNEMONIC_24)
 hd_wallet_spend = hd_wallet.derive_from_path("m/1852'/1815'/0'/0/0")
 spend_public_key = hd_wallet_spend.public_key
 spend_vk = PaymentVerificationKey.from_primitive(spend_public_key)
+pub_key_hash = spend_vk.hash()
 
 hd_wallet_stake = hd_wallet.derive_from_path("m/1852'/1815'/0'/2/0")
 stake_public_key = hd_wallet_stake.public_key
@@ -19,3 +20,4 @@ extended_signing_key = ExtendedSigningKey.from_hdwallet(hd_wallet_spend)
 owner_addr = Address(spend_vk.hash(), stake_vk.hash(), network)
 
 print(owner_addr)
+print(pub_key_hash)
